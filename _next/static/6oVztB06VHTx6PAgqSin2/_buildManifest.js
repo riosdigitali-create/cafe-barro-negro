430cf9f364594e0e5b1624ae5ae5383215975fc3
riosdigitali-create <riosdigitali@gmail.com>
2026-09-14 22:14:01 -0600

    Publish compiled Café Barro Negro site

---
+++ _next/static/6oVztB06VHTx6PAgqSin2/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/cafe-barro-negro//_next/:path+",
+        "destination": "/cafe-barro-negro/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
